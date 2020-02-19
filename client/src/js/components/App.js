798b04989d21db04608244f24bb13c2f5eca3696
ryanharlow <ryanharlow95@gmail.com>
2020-02-19 17:11:53 -0600

    add 404 page

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -1,14 +1,17 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './Navbar';
 import LandingPage from './LandingPage';
 import AddNewLocation from './AddNewLocation';
+import Stand from './Stand'
+import NoMatch from './NoMatch'
 import Footer from './Footer';
 import './App.css';
 import Map from './Map'
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 import { connect } from "react-redux";
 import { setLoggedIn } from "../actions/index";
+
 
 function mapDispatchToProps(dispatch) {
   return {
@@ -24,21 +27,25 @@ function App(props) {
 
   useEffect(() => {
     axios.get('/api/users')
-    .then(res => {
-      if(!res.data.loggedIn){
-        props.setLoggedIn(false);
-      }
-    }).catch(err => {
-      console.log(err);
-    })
+      .then(res => {
+        if (!res.data.loggedIn) {
+          props.setLoggedIn(false);
+        }
+      }).catch(err => {
+        console.log(err);
+      })
   }, []);
 
   return (
     <div className="App">
       <Navbar />
-      <Route exact path='/' component={LandingPage} />
-      <Route exact path='/map' component={Map} />
-      <Route exact path='/add' component={AddNewLocation} />
+      <Switch>
+        <Route exact path='/' component={LandingPage} />
+        <Route exact path='/map' component={Map} />
+        <Route exact path='/add' component={AddNewLocation} />
+        <Route path='/stand/:id' component={Stand} />
+        <Route component={NoMatch} />
+      </Switch>
       <Footer />
     </div>
   );
