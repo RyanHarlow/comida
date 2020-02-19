3a12ee7c60bbfca2835bf5a984286fd21c51b76d
ryanharlow <ryanharlow95@gmail.com>
2020-02-18 19:59:37 -0600

    finish api route to add location

--- api/db/places.js
+++ api/db/places.js
@@ -20,12 +20,27 @@ db
 }
 
 const addStand = async (request, response) => {
+  try{
   const username = request.session.user;
   const user = await db.query('SELECT * FROM person WHERE username = $1', [username]);
-  const {name, tags, rating, review} = request.body;
+  const {name, tags, rating, reviewText, lat, long} = request.body;
+  const reviewDate = Date.now();
   
-  const queryText = 'INSERT INTO stand (long, lat, name, tags) VALUES ($1, $2, $3, $4) RETURNING *'
-  const standValues = []
+  const standQueryText = 'INSERT INTO stand (long, lat, name, tags) VALUES ($1, $2, $3, $4) RETURNING *'
+  const standValues = [long, lat, name, [tags]];
+  
+ const stand = await db.query(standQueryText, standValues);
+ 
+ const reviewQueryText = 'INSERT INTO REVIEW (stars, text_content, user_id, stand_id, date) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+ const reviewValues = [rating, reviewText, user.rows[0].id, stand.rows[0].id, reviewDate]; 
+ const review = await db.query(reviewQueryText, reviewValues)
+  
+  response.send({success: 'Location Added'})
+
+}catch(err){
+    response.send({err})
+  }
+
 
 
 }
