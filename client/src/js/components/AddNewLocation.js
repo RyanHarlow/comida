8f5468d548402ef6146ea10c093707cef9aa3289
ryanharlow <ryanharlow95@gmail.com>
2020-02-18 18:26:13 -0600

    lift state from AddNewLocationForm to AddNewLocation

--- client/src/js/components/AddNewLocation.js
+++ client/src/js/components/AddNewLocation.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import AddNewLocationMap from './AddNewLocationMap';
 import AddNewLocationForm from './AddNewLocationForm';
 import './AddNewLocation.css';
+import axios from 'axios'
 
 
 const mapStateToProps = state => {
@@ -11,22 +12,44 @@ const mapStateToProps = state => {
 };
 
 
-class AddNewLocation extends Component {
-    
+function AddNewLocation(props) {
 
-    render() {
+    const [name, setName] = useState('');
+    const [tags, setTags] = useState('');
+    const [rating, setRating] = useState(5);
+    const [review, setReview] = useState('');
 
-        if (!this.props.isLoggedIn) {
-            return (<Redirect to="/" />)
-        }
-        else {
-            return (
-                <div className={'AddNewLocation'}>
+    const handleSubmit = () => {
+        const data = {name, tags, rating, review}
+        axios.post('/api/places', data)
+        .then(res => {
+            console.log(res);
+        }).catch(err => {
+            console.log(err);
+        })
+
+    }
+
+    if (!props.isLoggedIn) {
+        return (<Redirect to="/" />)
+    }
+    else {
+        return (
+            <div className={'AddNewLocation'}>
                 <AddNewLocationMap />
-                <AddNewLocationForm />
-                </div>
-            );
-        }
+                <AddNewLocationForm
+                    name={name}
+                    setName={setName}
+                    tags={tags}
+                    setTags={setTags}
+                    rating={rating}
+                    setRating={setRating}
+                    review={review}
+                    setReview={setReview}
+                    handleSubmit={handleSubmit} />
+            </div>
+        )
+
     }
 }
 
--- client/src/js/components/AddNewLocationForm.js
+++ client/src/js/components/AddNewLocationForm.js
@@ -1,52 +1,37 @@
 import React, { useState } from 'react';
 import './AddNewLocationForm.css'
 import StarSelector from './StarSelector'
-import axios from 'axios';
 
 const AddNewLocationForm = (props) => {
-    const [name, setName] = useState('');
-    const [tags, setTags] = useState('');
-    const [rating, setRating] = useState(5);
-    const [review, setReview] = useState('');
-
-    function handleSubmit(){
-        const data = {name, tags, rating, review}
-        axios.post('/api/places', data)
-        .then(res => {
-            console.log(res);
-        }).catch(err => {
-            console.log(err);
-        })
-    }
 
     return (
         <div className={'AddNewLocationForm'} >
             <div className="field">
                 <label className="label">Location Name</label>
                 <div className="control">
-                    <input className="input" type="text" placeholder="Stand Name" value={name} onChange={(e) => setName(e.target.value)} />
+                    <input className="input" type="text" placeholder="Stand Name" value={props.name} onChange={(e) => props.setName(e.target.value)} />
                 </div>
             </div>
             <div className="field">
                 <label className="label">Tags</label>
                 <div className="control">
-                    <input className="input" type="text" placeholder="Tags... E.g. late night, tortas, tacos" value={tags} onChange={(e) => setTags(e.target.value)} />
+                    <input className="input" type="text" placeholder="Tags... E.g. late night, tortas, tacos" value={props.tags} onChange={(e) => props.setTags(e.target.value)} />
                 </div>
             </div>
             <div className={"field"}>
                 <label className='label'>Rating</label>
                 <div className='control'>
-                    <StarSelector rating={rating} handleChange={setRating} />
+                    <StarSelector rating={props.rating} handleChange={props.setRating} />
                 </div>
             </div>
             <div className={"field"}>
                 <label className='label'>Review</label>
                 <div className='control'>
-                    <textarea className="textarea" placeholder="Write your review here" value={review} onChange={(e) => setReview(e.target.value)} ></textarea>
+                    <textarea className="textarea" placeholder="Write your review here" value={props.review} onChange={(e) => props.setReview(e.target.value)} ></textarea>
                 </div>
             </div>
             <div className="control">
-                <button onClick={handleSubmit} className="button is-link">Submit</button>
+                <button onClick={props.handleSubmit} className="button is-link">Submit</button>
             </div>
         </div>
     )
