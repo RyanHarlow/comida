3852ea4ea5f9dfbad24c868da3729c9b09cbdc13
ryanharlow <ryanharlow95@gmail.com>
2020-02-20 16:29:38 -0600

    add map to stand page

--- client/src/js/components/Stand.js
+++ client/src/js/components/Stand.js
@@ -1,25 +1,36 @@
-import React, {useState, useEffect} from 'react';
-import {useParams} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import StandMap from './StandMap';
 import axios from 'axios';
 
-function Stand(props){
-    let {id} = useParams();
-    let [place, setPlace] = useState(null);
-
+function Stand(props) {
+    let { id } = useParams();
+    let [place, setPlace] = useState({ lat: 19.4326, long: -99.1332 });
+    console.log(place);
     useEffect(() => {
         axios.get(`/api/places/${id}`)
-        .then(res => {
-            setPlace(res.data);
-            console.log(place);
-        }).catch(err => {
-            console.log(err);
-        })
-    }, []); 
+            .then(res => {
+                setPlace(res.data.success);
+            }).catch(err => {
+                console.log(err);
+            })
+    }, []);
 
-    return(
-        <div className={Stand}>
-            <StandMap />
+    return (
+        <div className={'Stand'}>
+            <StandMap longitude={place.long} latitude={place.lat} />
+            <section className="hero">
+                <div className="hero-body">
+                    <div className="container">
+                        <h1 className="title">
+                            Hero title
+      </h1>
+                        <h2 className="subtitle">
+                            Hero subtitle
+      </h2>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
--- client/src/js/components/StandMap.js
+++ client/src/js/components/StandMap.js
@@ -1,15 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
+import ReactMapGL, { Marker } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarker } from '@fortawesome/free-solid-svg-icons'
 
-class StandMap extends Component{
-    constructor(props){
+class StandMap extends Component {
+
+
+    constructor(props) {
         super(props);
+        this.state = {
+            viewport: {
+                width: '100vw',
+                height: '60vh',
+                latitude: this.props.latitude,
+                longitude: this.props.longitude,
+                zoom: 11
+            },
+        };
     }
 
-    render(){
-        <div>
-
-        </div>
+    render() {
+        return(
+        <ReactMapGL
+            {...this.state.viewport}
+            onViewportChange={(viewport) => {
+                this.setState({ viewport })
+            }}
+            mapboxApiAccessToken='pk.eyJ1Ijoicnlhbmhhcmxvdzk1IiwiYSI6ImNrNTJ1c2NtbDAxdzczZ250eGZ5ZXYyY2gifQ.2EN37mO-Fn8QNmu-FoOFDA'
+            mapStyle="mapbox://styles/mapbox/streets-v11">
+            
+            <Marker latitude={this.props.latitude} longitude={this.props.longitude} offsetLeft={-6} offsetTop={-20}><FontAwesomeIcon style={{color: 'red'}} icon={faMapMarker} /></Marker>
+        </ReactMapGL>
+        )
     }
 }
+
 
 export default StandMap;
